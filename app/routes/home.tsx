b20c34b301c77979cfb0f4fe2b6eda93fc02c5e6
ShivanshuShekhar <shivanshushekhar786@gmail.com>
2025-07-30 22:43:03 +0530

    Initialized Authenication & Puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,10 +2,21 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import { resumes } from "constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
 
 export function meta({}: Route.MetaArgs) {
+
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+
   return [
-    { title: "ResuMind" },
+    { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
