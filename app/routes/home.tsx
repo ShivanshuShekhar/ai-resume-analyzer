08b6945308192a0445630a89efb211c4376ef717
ShivanshuShekhar <shivanshushekhar786@gmail.com>
2025-07-30 12:18:20 +0530

    initialize home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-eading">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review your submissions and check your AI-powerd feedback.</h2>
+      </div>
+    </section>
+  </main>
 }
